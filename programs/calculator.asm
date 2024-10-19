c0609875b394f3618dfdfbf932d7676ff6ab4c2b
Ihor Savenko <csigorek@gmail.com>
2024-10-19 21:30:42 +0100

    upd calculator.asm

--- programs/calculator.asm
+++ programs/calculator.asm
@@ -1,17 +1,17 @@
-INIT [0x00] = 0b00000001 ; Add 1 to the memory
-INIT [0x01] = 0b00000001 ; b
+INIT [0x00] = 0b00000001 ; Add 1 to the memory at address 0x00
+INIT [0x01] = 0b00000001 ; Add 1 to the memory at address 0x01
 
-LOAD R1, [0x00]
-LOAD R2, [0x00]
+LOAD R1, [0x00] ; Load value from RAM to register
+LOAD R2, [0x00] ; Load value from RAM to register
 
-OUT R1
-OUT R2
+OUT R1 ; Print Register 1
+OUT R2 ; Print Register 2
 
-ADD R1, R2, R3 ; c = a + b
+ADD R1, R2, R3 ; Add register 1 and register 2 and save result to the register 3
 
-STORE R3, [0x20] ; Save c to RAM
+STORE R3, [0x20] ; Save register 3 value to RAM
 
-OUT R3 ; print c
+OUT R3 ; print reguster 3
 
 ; Cleaning memory and registers
 CLEAR [0x00]
@@ -21,4 +21,4 @@ CLEAR R1
 CLEAR R2
 CLEAR R3
 
-HALT
+HALT ; Exit
